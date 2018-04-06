4d3809e9de80774c98583d3e7c72a121b58736dd
Adrian Moore <stewsters@gmail.com>
2018-04-06 12:16:57 -0500

    Hud

--- src/main/kotlin/com/stewsters/sand/display/renderSystems/HudRenderSystem.kt
+++ src/main/kotlin/com/stewsters/sand/display/renderSystems/HudRenderSystem.kt
@@ -1,6 +1,7 @@
 package com.stewsters.sand.display.renderSystems
 
 import com.stewsters.sand.game.map.World
+import org.codetome.zircon.api.Position
 import org.codetome.zircon.api.screen.Screen
 
 class HudRenderSystem() {
@@ -8,6 +9,22 @@ class HudRenderSystem() {
 
     fun processSystem(gameMap: World, screen: Screen) {
 
+        val playerHealth = gameMap.player.health
+        if (playerHealth != null)
+            drawLine(screen, 0, "HP: ${playerHealth.cur}")
+
+       val inv = gameMap.player.inventory
+
+        if(inv!=null){
+            drawLine(screen, 1, "Torches: ${inv.torches}")
+            drawLine(screen, 2, "Rope: ${inv.ropes}")
+        }
+    }
+
+    fun drawLine(screen: Screen, height: Int, text: String) {
+        text.forEachIndexed { index, c ->
+            screen.setCharacterAt(Position.of(index,height),c)
+        }
 
     }
 }
